6f07156616405787ed01bd7e77dc270fa7965ad4
Vicemi <45047420+Vicemi@users.noreply.github.com>
2021-11-22 11:48:19 -0300

    Archivos Principales de prueba
    Se han subido los primeros archivos para hacer las pruebas

---
+++ script.sh
@@ -0,0 +1,4 @@
+echo "El Script comenzara en breve a instalar todos los archivos necesarios para traducir su ControlPanel"
+sleep 5
+cd /var/www/dashboard/
+git clone https://github.com/Vicemi/ControlPanel-en-Spanish.git
