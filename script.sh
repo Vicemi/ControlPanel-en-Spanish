9b3bb75926cb67f563976ce45a3cec02007af329
Vicemi <45047420+Vicemi@users.noreply.github.com>
2021-11-22 11:51:19 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 echo "El Script comenzara en breve a instalar todos los archivos necesarios para traducir su ControlPanel"
 sleep 5
 cd /var/www/dashboard/
-git clone https://github.com/Vicemi/ControlPanel-en-Spanish.git
+git clone https://github.com/Vicemi/ControlPanel-en-Spanish/blob/6f07156616405787ed01bd7e77dc270fa7965ad4/Prueba.txt
